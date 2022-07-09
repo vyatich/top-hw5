6554f554e95f7af3ef348eda6bc7c84cebaf4196
vyatich <vik_ivy@mail.ru>
2022-07-09 13:04:13 +0300

    Initial commit

---
+++ src/main/java/com/example/tophw5/TopHw5Application.java
@@ -0,0 +1,13 @@
+package com.example.tophw5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopHw5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopHw5Application.class, args);
+    }
+
+}
